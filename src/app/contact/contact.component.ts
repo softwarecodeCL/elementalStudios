d73514c8108a22157a43f0936420ac4181ab41b6
Marco Estay <m.estayroman@gmail.com>
2024-11-27 00:54:35 -0300

    up

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule,
+  imports: [CommonModule, HttpClientModule,
             ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
@@ -16,7 +17,7 @@ export class ContactComponent {
   userForm : FormGroup;
   captchaResponse: string | null = null;
   
-  constructor() {
+  constructor(private http: HttpClient) {
     this.userForm = new FormGroup({
       firstName: new FormControl('',[Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -29,17 +30,32 @@ export class ContactComponent {
     })
   }
 
-  onSubmit(e: any) {
-   
-    
-    alert("asdasdasd")
-    e.preventDefault();
+  onSubmit() {
+   /*
     if (this.userForm.valid) {
-      console.log(this.userForm.value);
-      // Maneja el envío del formulario aquí, incluyendo el captchaResponse si es necesario
+      const formData = { // Reemplaza con la dirección de destino adecuada
+        subject: 'Nuevo mensaje de contacto',
+        text: `
+          Nombre: ${this.userForm.get('firstName')?.value}
+          Email: ${this.userForm.get('email')?.value}
+          Mensaje: ${this.userForm.get('message')?.value}
+        `,
+        captcha: this.userForm.get('recaptcha')?.value
+      };
+
+      this.http.post('https://tu-servidor/api/enviar-correo', formData)
+        .subscribe({
+          next: (response) => {
+            console.log('Correo enviado con éxito:', response);
+            // Maneja la respuesta del servidor aquí (e.g., muestra un mensaje de éxito)
+          },
+          error: (error) => {
+            console.error('Error al enviar el correo:', error);
+          }
+        });
     } else {
       console.log('Formulario no válido');
-    }
+    } */
   }
 
   onCaptchaResolved(captchaResponse: string | null) {
